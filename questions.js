66087fd9761724a05d5706071faec03a6b3e98f4
Johirul Khalid <111776170+johirulkhalid@users.noreply.github.com>
2022-08-28 17:53:35 +0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answer
+let questions = [
+    {
+        num: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language",
+        ]
+    },
+    {
+        num: 2,
+        question: "What does CSS stand for?",
+        answer: "Casecading Style Sheet",
+        options: [
+            "Comon Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Casecading Style Sheet",
+        ]
+    },
+    {
+        num: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogmming",
+            "Hometext Preprocessor",
+        ]
+       
+    },
+    {
+        num: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylise Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language",
+        ]
+    },
+    {
+        num: 5,
+        question: "What does XML stand for?",
+        answer: "Extensible Markup Language",
+        options: [
+            "Extensible Markup Language",
+            "eXecutable Multiple Language",
+            "X-Markup Language",
+            "EXtra Modern Link",
+        ]
+    }
+];
